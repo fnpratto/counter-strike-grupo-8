282bc5c891ce4207ca3545e94332a864bea14b84
fnpratto <fnpratto@gmail.com>
2025-05-09 15:28:41 -0300

    feat: add hud pointer and money

--- gui/main.cpp
+++ gui/main.cpp
@@ -6,16 +6,20 @@
 #include "window_elements/SdlTexture.h"
 #include "window_elements/SdlWindow.h"
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
 int main(int argc, char** argv) {
     try {
-        SdlWindow window(800, 600);
-        window.fill();
-        SdlTexture im("assets/gfx/aztec.bmp", window);  // load background
+        SdlWindow window(SCREEN_WIDTH, SCREEN_HEIGHT);
+        SdlTexture back("assets/gfx/background.bmp", window);    // load background
+        SdlTexture pointer("assets/gfx/pointer.xcf", window);    // load  pointer
+        SdlTexture money("assets/gfx/hud_symbols.xcf", window);  // load  pointer
         SDL_Event e;
-        Area srcArea(0, 0, 300, 300);
+        Area SizePointer(1, 1, 44, 44);      // hud
+        Area SizeBackground(35, 0, 30, 30);  // background
+        Area SizeMoney(460, 0, 60, 60);      // money
         bool quit = false;
-        int x = 100;
-        int y = 150;
         while (!quit) {
             // Handle events on queue
             while (SDL_PollEvent(&e) != 0) {
@@ -25,9 +29,16 @@ int main(int argc, char** argv) {
                 }
             }
 
-            Area destArea(x, y, 300, 300);
+            Area destArea(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 28,
+                          SCREEN_HEIGHT / 28);
+            Area destAreaBackground(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            Area destAreaMoney(50, SCREEN_HEIGHT - 50, SCREEN_WIDTH / 28, SCREEN_HEIGHT / 28);
             window.fill();  // Repinto el fondo gris
-            im.render(srcArea, destArea);
+
+            back.render(SizeBackground, destAreaBackground);
+            pointer.render(SizePointer, destArea);
+            money.render(SizeMoney, destAreaMoney);
+
             // poll event
             window.render();
         }
