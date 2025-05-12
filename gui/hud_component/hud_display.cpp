d631ac82d480208642864908a5714046b61ae025
fnpratto <fnpratto@gmail.com>
2025-05-12 15:36:45 -0300

    feat: score of teams

--- gui/hud_component/hud_display.cpp
+++ gui/hud_component/hud_display.cpp
@@ -3,42 +3,32 @@
 #include <algorithm>
 #include <filesystem>
 #include <vector>
+
+// Constants
 const int size_width = 62;
 const int size_height = 64;
 const int padding = 10;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-float scale = 0.5f;
-int digitSpacingSmall = static_cast<int>(42 * scale) + 1;
-int digitSpacingMedium = static_cast<int>(42) + 1;
+const float scale = 0.5f;
+const int digitSpacingSmall = static_cast<int>(42 * scale) + 1;
 
-const Area sizeMoney = Area(460, 0, size_height, size_width);
-const Area sizeChronometer = Area(130, 0, size_height, size_width);
-const Area sizeLife = Area(0, 0, size_height, size_width);
-const Area sizeBullets = Area(0, 0, 300, 302);
-const Area sizePointer = Area(0, 0, 50, 50);
-const Area destPointer = Area(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, 50);
-const Area sizeBackground = Area(0, 0, 60, 60);
-const Area destBackground = Area(0, 0, 800, 600);
-
-
-const Area destMoney = Area(SCREEN_WIDTH - size_width - padding * 2 - SCREEN_WIDTH / 20,
-                            SCREEN_HEIGHT - size_height + 10, SCREEN_WIDTH / 20, 32);
-const Area destLife = Area(10, SCREEN_HEIGHT - size_height + 10, SCREEN_WIDTH / 20, 32);
-const Area destBullets = Area(SCREEN_WIDTH - padding * 5, SCREEN_HEIGHT - size_height * 2 + 10,
-                              SCREEN_WIDTH / 20, 32);
-
-
+// Paths
 const std::string& BACKGROUND_PATH = "assets/gfx/backgrounds/water1.jpg";
 const std::string& POINTER_PATH = "assets/gfx/hud/pointer.xcf";
 const std::string& MONEY_PATH = "assets/gfx/hud/hud_symbols.xcf";
 const std::string& LIFE_PATH = "assets/gfx/hud/hud_symbols.xcf";
-const std::string& CHRONOMETER_PATH = "assets/gfx/hud/hud_symbols.xcf";
 const std::string& FONT_PATH = "assets/gfx/fonts/joystix_monospace.otf";
+const std::string& BULLET_PATH = "assets/gfx/hud/bullet-icon1.xcf";
 const std::string& TRAPECIO_PATH = "assets/gfx/hud/trapezoid.xcf";
-const std::string& BULLET_PATH = "assets/gfx/hud/bullet-icon1.xcf";  // TODO color
+const std::string& HUD_NUMS_PATH = "assets/gfx/fonts/hud_nums.xcf";
+const std::string& PARALELO_RED_PATH = "assets/gfx/hud/parallelogram.xcf";
+const std::string& PARALELO_BLUE_PATH = "assets/gfx/hud/parallelogram_blue.xcf";
+const std::string& PARALELO_RED_O_PATH = "assets/gfx/hud/parallelogram_red_op.xcf";
+const std::string& PARALELO_BLUE_O_PATH = "assets/gfx/hud/parallelogram_blue_op.xcf";
 
 
+// Constructor
 hudDisplay::hudDisplay(SdlWindow& window):
         window(window),
         back(BACKGROUND_PATH, window),
@@ -52,126 +42,179 @@ hudDisplay::hudDisplay(SdlWindow& window):
         roundText(FONT_PATH, 20, {150, 150, 150, 255}, window),
         equipedBullets(BULLET_PATH, window),
         equipedBulletsAmount(window.getRenderer(), "assets/gfx/fonts/hud_nums.xcf"),
-        gunNumber(FONT_PATH, 20, {150, 150, 150, 255}, window) {}
+        gunNumber(FONT_PATH, 20, {150, 150, 150, 255}, window),
+        scoreText(FONT_PATH, 20, {255, 255, 255, 255}, window) {}
 
+// Main render function
 void hudDisplay::render() {
     window.fill();
-    back.render(sizeBackground, destBackground);
+    renderBackground();
+    renderParal();
+    renderPointer();
+    renderMoney();
+    renderLife();
+    renderTimer();
+    renderRoundText();
+    renderBullets();
+    renderGunIcons();
+    window.render();  // Swap buffers
+}
 
+// Render background
+void hudDisplay::renderBackground() {
+    const Area sizeBackground(0, 0, 60, 60);
+    const Area destBackground(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    back.render(sizeBackground, destBackground);
     SDL_Renderer* renderer = window.getRenderer();
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
     SDL_Rect rect = {0, 0, SCREEN_WIDTH, 10};
     SDL_RenderFillRect(renderer, &rect);
-
     SdlTexture trapecio(TRAPECIO_PATH, window);
     Area srcTrapecio(0, 0, 500, 230);
     Area destTrapecio(SCREEN_WIDTH / 2 - 200, 0, SCREEN_WIDTH / 2, size_height * 2);
     trapecio.render(srcTrapecio, destTrapecio);
-
-
-    pointer.render(sizePointer, destPointer);
-    updateComponents();
-    money.render(sizeMoney, destMoney);
-    std::string moneyStr = "1000";  // Just the numeric part
-    int x = SCREEN_WIDTH - size_width - padding * 4;
-    int y = SCREEN_HEIGHT - size_height + 10;
-    for (char c: moneyStr) {
-        if (isdigit(c)) {
-            money_amount.renderDigit(c - '0', x, y, scale);
-            x += digitSpacingSmall;
-        }
-    }
-
-    life.render(sizeLife, destLife);
-    std::string lifeStr = "100";  // Just the numeric part
-    x = 55;
-    y = SCREEN_HEIGHT - size_height + 10;
-    for (char c: lifeStr) {
-        if (isdigit(c)) {
-            life_amount.renderDigit(c - '0', x, y, scale);
-            x += digitSpacingSmall;
-        }
-    }
-    show_timer();
-    roundText.setTextString("Round 10");
-    roundText.render(Area(SCREEN_WIDTH / 2 - 50, padding, 100, 20));
-
-    equipedBullets.render(sizeBullets, destBullets);
-
-    std::string bulletsStr = "30";  // Just the numeric part
-    x = SCREEN_WIDTH - size_width - padding * 6;
-    y = SCREEN_HEIGHT - size_height * 2 + 10;
-    for (char c: bulletsStr) {
-        if (isdigit(c)) {
-            equipedBulletsAmount.renderDigit(c - '0', x, y, scale);
-            x += digitSpacingSmall;
-        }
-    }
-
-    renderGunIcons();
-
-    window.render();  // swap buffers
 }
 
-void hudDisplay::show_timer() {
+// Render trapecio
+void hudDisplay::renderParal() {
 
+    // int randomNum = getTeam()  // TODO logica depende el usuario si es terrorista o counter
+    // if (randomNum == 1) {
+    SdlTexture parallelogram1(PARALELO_BLUE_O_PATH, window);
+    Area srcParallelogram1(0, 180, 500, 320);
+    Area destParallelogram1(SCREEN_WIDTH / 2 - 290, padding * 2, size_width * 3, size_height * 2);
+    parallelogram1.render(srcParallelogram1, destParallelogram1);
+
+
+    scoreText.setTextString("9");
+    scoreText.render(Area(SCREEN_WIDTH / 2 - 200, padding * 3, size_width / 1.75, size_height / 2));
+
+    SdlTexture parallelogram(PARALELO_RED_PATH, window);
+    Area srcParallelogram(0, 180, 500, 320);
+    Area destParallelogram(SCREEN_WIDTH / 2 + 110, padding * 2, size_width * 3, size_height * 2);
+    parallelogram.render(srcParallelogram, destParallelogram);
+
+    scoreText.setTextString("1");
+    scoreText.render(Area(SCREEN_WIDTH / 2 + 190, padding * 3, size_width / 1.75, size_height / 2));
+    /*}
+    else {
+        SdlTexture parallelogram(PARALELO_BLUE_PATH, window);
+        Area srcParallelogram(0, 0, 500, 230);
+        Area destParallelogram(0, 0, 100, size_height * 2);
+        parallelogram.render(srcParallelogram, destParallelogram);
+
+        SdlTexture parallelogram1(PARALELO_RED_O_PATH, window);
+        Area srcParallelogram1(0, 0, 500, 230);
+        Area destParallelogram1(SCREEN_WIDTH / 2 - 200, 0, 100, size_height * 2);
+        parallelogram1.render(srcParallelogram1, destParallelogram1);
+        SDL_Renderer* renderer = window.getRenderer();
+    }*/
+}
+
+// Render pointer
+void hudDisplay::renderPointer() {
+    const Area sizePointer(0, 0, 50, 50);
+    const Area destPointer(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, 50);
+    pointer.render(sizePointer, destPointer);
+}
+
+// Render money
+void hudDisplay::renderMoney() {
+    const Area sizeMoney(460, 0, size_height, size_width);
+    const Area destMoney(SCREEN_WIDTH - size_width - padding * 2 - SCREEN_WIDTH / 20,
+                         SCREEN_HEIGHT - size_height + 10, SCREEN_WIDTH / 20, 32);
+    money.render(sizeMoney, destMoney);
+
+    std::string moneyStr = "1000";
+    int x = SCREEN_WIDTH - size_width - padding * 4;
+    int y = SCREEN_HEIGHT - size_height + 10;
+    renderDigits(moneyStr, x, y, money_amount);
+}
+
+// Render life
+void hudDisplay::renderLife() {
+    const Area sizeLife(0, 0, size_height, size_width);
+    const Area destLife(10, SCREEN_HEIGHT - size_height + 10, SCREEN_WIDTH / 20, 32);
+    life.render(sizeLife, destLife);
+
+    std::string lifeStr = "100";
+    int x = 55;
+    int y = SCREEN_HEIGHT - size_height + 10;
+    renderDigits(lifeStr, x, y, life_amount);
+}
+
+// Render timer
+void hudDisplay::renderTimer() {
     int currentClockTick = 123;
     int minutesIdx = std::floor(currentClockTick / 60);
     int seconds = currentClockTick % 60;
     int secondsIdxH = std::floor(seconds / 10);
     int secondsIdxL = seconds % 10;
 
-    // Render minutes
     int x = SCREEN_WIDTH / 2 - 40;
     int y = padding + 30;
+
     timer_amount.renderDigit(minutesIdx, x, y, scale);
     x += digitSpacingSmall;
 
-    // Render colon
     Area srcColon(475, 0, 10, size_height);
     Area dstColon(x, y, 10, size_height / 2);
     timer_dots.render(srcColon, dstColon);
     x += digitSpacingSmall;
 
-    // Render tens place of seconds
     timer_amount.renderDigit(secondsIdxH, x, y, scale);
     x += digitSpacingSmall;
-
-    // Render units place of seconds
     timer_amount.renderDigit(secondsIdxL, x, y, scale);
 }
 
+// Render round text
+void hudDisplay::renderRoundText() {
+    roundText.setTextString("Round 10");
+    roundText.render(Area(SCREEN_WIDTH / 2 - 50, padding, 100, 20));
+}
 
-void hudDisplay::renderGunIcons() {  // TODO change to user state
+// Render bullets
+void hudDisplay::renderBullets() {
+    const Area sizeBullets(0, 0, 300, 302);
+    const Area destBullets(SCREEN_WIDTH - padding * 5, SCREEN_HEIGHT - size_height * 2 + 10,
+                           SCREEN_WIDTH / 20, 32);
+    equipedBullets.render(sizeBullets, destBullets);
+
+    std::string bulletsStr = "30";
+    int x = SCREEN_WIDTH - size_width - padding * 6;
+    int y = SCREEN_HEIGHT - size_height * 2 + 10;
+    renderDigits(bulletsStr, x, y, equipedBulletsAmount);
+}
+
+// Render gun icons
+void hudDisplay::renderGunIcons() {
     int x = SCREEN_WIDTH - size_width - padding * 6;
     int y = SCREEN_HEIGHT / 2;
     int spacing = 64;
 
-    SdlTexture ak47("assets/gfx/guns/ak47_k.xcf", window);
-    SdlTexture aug("assets/gfx/guns/aug_k.xcf", window);
-    SdlTexture elite("assets/gfx/guns/elite_k.xcf", window);
+    renderGunIcon("assets/gfx/guns/ak47_k.xcf", "1", x, y);
+    y += spacing;
+    renderGunIcon("assets/gfx/guns/aug_k.xcf", "2", x, y);
+    y += spacing;
+    renderGunIcon("assets/gfx/guns/elite_k.xcf", "3", x, y);
+}
 
+// Helper to render gun icon
+void hudDisplay::renderGunIcon(const std::string& path, const std::string& number, int x, int y) {
+    SdlTexture gunTexture(path, window);
     Area destArea(x, y, SCREEN_WIDTH / 10, 34);
-    ak47.render(Area(0, 0, 30, 10), destArea);
-    gunNumber.setTextString("1");
-    gunNumber.render(Area(SCREEN_WIDTH - padding * 2, y, 10, 20));
-    y += spacing;
-
-    destArea = Area(x, y, SCREEN_WIDTH / 10, 34);
-    aug.render(Area(0, 0, 28, 10), destArea);
-    gunNumber.setTextString("2");
-    gunNumber.render(Area(SCREEN_WIDTH - padding * 2, y, 10, 20));
-    y += spacing;
-
-    destArea = Area(x, y, SCREEN_WIDTH / 10, 34);
-    elite.render(Area(0, 0, 15, 10), destArea);
-    gunNumber.setTextString("3");
+    gunTexture.render(Area(0, 0, 30, 10), destArea);
+    gunNumber.setTextString(number);
     gunNumber.render(Area(SCREEN_WIDTH - padding * 2, y, 10, 20));
 }
 
-void hudDisplay::updateComponents() {
-    /*money_amount.setTextString("$100");
-    life_amount.setTextString("Life: 100");
-    timer_amount.setTextString("Chrono: 00:00");*/
+// Helper to render digits
+void hudDisplay::renderDigits(const std::string& str, int x, int y, BitmapFont& texture) {
+    for (char c: str) {
+        if (isdigit(c)) {
+            texture.renderDigit(c - '0', x, y, scale);
+            x += digitSpacingSmall;
+        }
+    }
 }
--- gui/hud_component/hud_display.h
+++ gui/hud_component/hud_display.h
@@ -24,19 +24,29 @@ private:
     BitmapFont money_amount;
     SdlTexture life;
     BitmapFont life_amount;
-    /*SdlTexture equipedGuns;
-    BitmapFont equipedGuns_amount;*/
     SdlTexture equipedBullets;
     BitmapFont equipedBulletsAmount;
     BitmapFont timer_amount;
     SdlTexture timer_dots;
     SdlText roundText;
     SdlText gunNumber;
+    SdlText scoreText;
+    void renderBackground();
+    void renderParal();
+    void renderPointer();
+    void renderMoney();
+    void renderLife();
+    void renderTimer();
+    void renderRoundText();
+    void renderBullets();
     void renderGunIcons();
 
     void show_timer();
 
     void updateComponents();
+
+    void renderGunIcon(const std::string& path, const std::string& number, int x, int y);
+    void renderDigits(const std::string& str, int x, int y, BitmapFont& texture);
 };
 
 #endif  // HUD_DISPLAY_H
