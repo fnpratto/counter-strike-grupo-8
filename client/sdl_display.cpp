623a6dddbc7ba8c5808dcd557861240c29f83aac
fnpratto <fnpratto@gmail.com>
2025-05-19 18:18:41 -0300

    fix in main client

--- client/sdl_display.cpp
+++ client/sdl_display.cpp
@@ -4,13 +4,42 @@
 #include <memory>
 #include <string>
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+
 #include "common/message.h"
+#include "gui/hud_component/hud_display.h"
+#include "gui/window_elements/SdlWindow.h"
 
 #include "display.h"
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-// TODO: Implement SDLDisplay
 
 SDLDisplay::SDLDisplay(Queue<Message>& input_queue, Queue<Message>& output_queue):
         Display(input_queue, output_queue) {}
-void SDLDisplay::run() {}
+void SDLDisplay::run() {
+    try {
+        SdlWindow window(SCREEN_WIDTH, SCREEN_HEIGHT);
+        hudDisplay hudDisplay(window);
+
+        SDL_Event e;
+        bool quit = false;
+        while (!quit) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+            }
+
+            hudDisplay.render();
+        }
+
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return;
+    }
+    return;
+}
+
 void SDLDisplay::stop() { Thread::stop(); }
